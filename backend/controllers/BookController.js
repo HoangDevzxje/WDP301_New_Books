affe9637db90f654417ac6af6c32fe3c39515a48
Minh Dong <nvdong0902@gmail.com>
2025-06-06 00:13:24 +0700

    commit bookController

---
+++ backend/controllers/BookController.js
@@ -0,0 +1,38 @@
+const Book = require("../models/Book");
+const Category = require("../models/Category");
+
+const getAllBooks = async (req, res) => {
+    try {
+        const books = await Book.find({ isActivated: true });
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server!", error: error.message });
+    }
+};
+
+const getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) return res.status(404).json({ message: "Không tìm thấy sách" });
+        res.status(200).json(book);
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi khi lấy sách", error: error.message });
+    }
+}
+
+const getBookByCategory = async (req, res) => {
+    try {
+        const books = await Book.find({ categories: req.params.id });
+        if (books.length === 0) return res.status(404).json({ message: "Không tìm thấy sách" });
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server!", error: error.message });
+    }
+};
+
+
+module.exports = {
+    getAllBooks,
+    getBookById,
+    getBookByCategory
+}
