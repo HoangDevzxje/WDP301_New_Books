9726f59fef45c04cce7db077b2d8ccc795a05f42
HoangDevzxje <pk19032003@gmail.com>
2025-06-08 20:55:42 +0700

    Cms book, category book

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
-const authRoutes = require('./authRoute')
-const bookRoutes = require('./bookRoute')
+const authRoutes = require("./authRoute");
+const bookRoutes = require("./bookRoute");
+const adminRoutes = require("./adminRoute");
 
 const routes = (app) => {
-    app.use('/auth', authRoutes)
-    app.use('/book', bookRoutes)
-}
+  app.use("/auth", authRoutes);
+  app.use("/book", bookRoutes);
+  app.use("/admin", adminRoutes);
+};
 
-module.exports = routes
+module.exports = routes;
