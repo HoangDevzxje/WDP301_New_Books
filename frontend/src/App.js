0cc2b7b3f42d5409b9cb75484b6e7dfe0eadf648
anhnguyen52 <154457042+anhnguyen52@users.noreply.github.com>
2025-06-12 10:07:19 +0700

    App - package - logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
-  
+  const location = useLocation();  ;
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
   return (
     <>
-      
+      {!isAdminRoute &&
+        <Header/>
+      }
+
+       <Routes>
+         <Route path="/" element={<HomePage/>} />
+       </Routes>
+
+       {!isAdminRoute && 
+        <Footer />
+       }
     </>
   );
 }
