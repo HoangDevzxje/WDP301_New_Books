e9eca01cfd81f77f14caed9caa468b897db6b497
sieunhandong <dongnvhe172730@fpt.edu.vn>
2025-07-10 23:52:27 +0700

    commit review be

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,6 +11,7 @@ const orderRoutes = require("./orderRoute");
 const addressRoutes = require("./addressRoute");
 const ghnRoutes = require("./ghnRoute");
 const paymentRoutes = require("./paymentRoute");
+const reviewRoutees = require("./reviewRoute");
 const routes = (app) => {
   app.use("/auth", authRoutes);
   app.use("/book", bookRoutes);
@@ -25,6 +26,7 @@ const routes = (app) => {
   app.use("/addresses/users/:userId", addressRoutes);
   app.use("/ghn", ghnRoutes);
   app.use("/payment", paymentRoutes);
+  app.use("/review", reviewRoutees);
 };
 
 module.exports = routes;
